618042923beb1a09f08e9f628dfe1026c6364249
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2018-07-18 21:15:01 +0530

    Create day_22bst.py

---
+++ day_22bst.py
@@ -0,0 +1,40 @@
+	void levelOrder(Node * root){
+    int h = height(root);
+    int i;
+    for (i=1; i<=h; i++)
+        printGivenLevel(root, i);
+}
+ 
+/* Print nodes at a given level */
+void printGivenLevel(Node * root, int level)
+{
+    if (root == NULL)
+        return;
+    if (level == 1)
+        printf("%d ", root->data);
+    else if (level > 1)
+    {
+        printGivenLevel(root->left, level-1);
+        printGivenLevel(root->right, level-1);
+    }
+}
+ 
+/* Compute the "height" of a tree -- the number of
+    nodes along the longest path from the root node
+    down to the farthest leaf node.*/
+int height(Node * node)
+{
+    if (node==NULL)
+        return 0;
+    else
+    {
+        /* compute the height of each subtree */
+        int lheight = height(node->left);
+        int rheight = height(node->right);
+ 
+        /* use the larger one */
+        if (lheight > rheight)
+            return(lheight+1);
+        else return(rheight+1);
+    }
+}
